4ff9d9a54e55716e15ddee9165fd322ec249281a
henry-israel <henryisrael08@gmail.com>
2022-02-15 09:40:38 -0500

    adds in processing tool kit, basic data processing implemented, probably ought to move to its own class

--- mcinputgeneration.py
+++ mcinputgeneration.py
@@ -1,10 +1,13 @@
+import string
 import numpy as np
 import scipy.stats
+import pandas as pd
 from matplotlib import pyplot as plt
 import multiprocessing as mp
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
+import time
 
 
 class mcmc:
@@ -124,8 +127,8 @@ class mcmc:
         stepcount=1
         self._numberaccepted=1
         while stepcount<nsteps:
-            if stepcount%np.floor(nsteps/10)==0:
-                print(f"Completed {stepcount}/{nsteps} steps, accepted {self._numberaccepted}")
+            # if stepcount%np.floor(nsteps/10)==0:
+            #     print(f"Completed {stepcount}/{nsteps} steps, accepted {self._numberaccepted}")
             
             
             prop_step=self.proposeStep(curr_step)
@@ -138,6 +141,8 @@ class mcmc:
             self._acceptedsteps[stepcount]=curr_step
             self._acceptedllhs[stepcount]=curr_llh
             
+
+
             stepcount+=1
         return self._acceptedsteps, self._acceptedllhs
     
@@ -172,14 +177,25 @@ class mcmc:
         return num_k/denom_k
         
 class mcmc_training_gen():
-    def __init__(self,trainsize=10000, mcmcdim=10, mcmcsteps=10000, autocorrlag=1000):
+    def __init__(self,trainsize=10, mcmcdim=10, mcmcsteps=10000, autocorrlag=1000, mcmc_stepsizes=None):
         
         self._trainsize=trainsize #amount of training data we want
         self._mcmcdim=mcmcdim #mcmc dimension
         self._mcmcsteps=mcmcsteps #mcmc steps to run
         self._autocorrlag=autocorrlag #lag on autocorrelations
+        self._mcmc_stepsizes=mcmc_stepsizes
+        self._appenddata=False #Append to previous array when running
         self._trainarr=[]
+        
+        self._processed_data=pd.DataFrame()     
+        #Processing stuff
+
+    def getMCMCStepSizes(self):
+        return self._mcmc_stepsizes
     
+    def updateMCMCStepSizes(self,updated_vals):
+        self._mcmc_stepsizes=updated_vals
+        return 1
 
     def getMCMCDim(self):
         return self._mcmcdim
@@ -202,10 +218,16 @@ class mcmc_training_gen():
     def getAutoCorrLag(self):
         return self._autocorrlag
 
-    def updateTrainData(self, update_arr,append_vals=False):
+    def updateTrainSize(self,updateval):
+        self._trainsize=updateval
+        return 1
+
+
+    def updateTrainData(self, update_arr):
         #Updates the array of training data
         #can either append values or overwrite
-        if append_vals:
+        if self._appenddata:
+            self.updateTrainSize(self._trainsize+len(update_arr))
             self._trainarr.append(update_arr)
         else:
             self._trainarr=update_arr
@@ -218,28 +240,93 @@ class mcmc_training_gen():
         #generate mcmc instance
         #i just a placeholder!
         mc=mcmc(self._mcmcdim)
-        mc_acc, mc_llh=mc(np.random.randn(self._mcmcdim),nsteps=self._mcmsteps)
+        mc_acc, mc_llh=mc(np.random.randn(self._mcmcdim),nsteps=self._mcmcsteps,stepsize=self._mcmc_stepsizes)
         mcauto=mc.autocorrs(self._autocorrlag)
         return [mc_acc, mc_llh, mcauto]
     
-    def __call__(self,append_vals=True):
+    def generateTrainingData(self):
         b_pool=mp.Pool()
         train_arr=b_pool.map(self.createTrain, range(self._trainsize))
         # train_arr=[]
         # for i in range(self.trainsize):
         #     train_arr.append(self.createTrain(i))
-        self.updateTrainData(train_arr,append_vals)
+        self.updateTrainData(train_arr)
         return train_arr
+    
+    #Training Data Processing
+    def binMeansDataSet(self, indata, nbins=20): 
+        #Splits data into nbins, then takes mean
+        return [np.mean(x) for x in np.split(indata,nbins)]
+
+    def binMaxDataSet(self,indata,nbins=20):
+        #Splits data into nbins, then takes max of each bin
+        return [np.max(x) for x in np.split(indata, 20)]
+
+    def integrateData(self, indata, nbins=30):
+        #Integrate data numerically by splitting in number of bins (also normalises it!)
+        hbins=self.binMaxDataSet(indata,nbins)
+        return np.sum(hbins)/nbins
+
+    def getNthElements(self,indata,nelement):
+        #Grabs every nth entry
+        return [x[0] for x in np.split(indata,nelement)]
+    #Can add more metrics where necessary!
+
+    def updateProcessedData(self,updateddata):
+        if self._appenddata:
+            if updateddata.top() != self._processed_data.top:
+                raise ValueError("headers for updated data do not match those of already present data!")
+            pd.concat(self._processed_data,updateddata)
+        else:
+            self._processed_data=updateddata
+        return 1
+    
+    def getProcessedData(self):
+        return self._processed_data
+
+    def processTrainData(self, llhbins=20, autocorr_integral_split=20, autocorr_reduce_size=50,append_data=False):
+
+        self._appenddata=append_data
+        trset=np.array(self._trainarr,dtype=object)
+#        print(trset)
+#        print(len(trset))
+        mc_acc=trset[:,0]
+        mc_llh=trset[:,1]
+        mc_auto=trset[:,2]
+
+        #For now let's do mean-binning for llhs
+        processed_llh=[self.binMeansDataSet(i,llhbins) for i in mc_llh]
+        #Autocorrs require a different treatment
+        integrateautocorr=[self.integrateData(i,autocorr_integral_split) for i in mc_auto]
+        #Reduce number of points (Mean pooling may smear useful features, will need investigating!)
+        reduced_autocorr=[self.getNthElements(i, autocorr_reduce_size) for i in mc_auto]
+
+        #Let's make a massive dataframe!
+        process_df=pd.DataFrame()
+        process_df["Acceptance Rate"]=mc_acc
+        process_df["Integrated Autocorrelation"]=integrateautocorr
+        process_df["Reduced Auto Corr"]=reduced_autocorr
+        process_df["Binned LLH"]=processed_llh
+
+        self.updateProcessedData(process_df)
+        return process_df
+
+    def __call__(self, llhbins=20, autocorr_integral_split=20, autocorr_reduce_size=50,append_data=False):
+        #llhbins = number of llh bins
+        #autocorr_integral_split=number of bins to split autocorr data into before integration
+        #autocorr_reduce_size= how much to reduce autocorrelation by
+        #append_data -> append data?
+        self.generateTrainingData()
+        self.processTrainData(llhbins, autocorr_integral_split, autocorr_reduce_size, append_data)
+        print("Processed data set has been created!")
         
+
     #Here's the idea:
         # Generate step size
         # 1 run standard NN training 
         # Whoever win the NN decides the step size
         # Need way to shrink variance of each step in a smart way
-      
-
-
-
 if __name__== "__main__":
-    x=mcmc_ml(2)
-    trset = x()
+    traindata=mcmc_training_gen(2,autocorrlag=200)
+    traindata()
+    print(traindata.getProcessedData())
